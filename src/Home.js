854aeede328dec7c733952ecfab6c3accdc550cb
Nikita Kortunov <smith@MacBook-Pro-Nikita.local>
2022-08-04 15:56:13 +0400

    Fixed

--- src/Home.js
+++ src/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
           const progress = a[word.word] ? a[word.word].progress : 0
           const shows = a[word.word] ? a[word.word].schows : 0
           const r = (a[word.word] && !isNaN(progress/shows) ? progress/shows : 0) * 100
-          globalProgress[group] += shows > (maxShow / 10) && r >= 95 ? 1: 0
+          globalProgress[group] += shows > (maxShow / 10) && shows > 5 && r >= 95 ? 1: 0
         })
       })
       setProgress(globalProgress)
